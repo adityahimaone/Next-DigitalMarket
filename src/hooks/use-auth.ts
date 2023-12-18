8479063fa1bc1cfd53eaaed3ae8800635e30f933
adityahimaone <adityahimaone@gmail.com>
2023-12-18 22:11:12 +0700

    feat: add user account dropdown menu and sign out hooks

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,33 @@
+import { router } from "@/trpc/trpc";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export const useAuth = () => {
+  const router = useRouter();
+  const signOut = async () => {
+    try {
+      // /api/users/logout provided by CMS
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!res.ok) throw new Error("Failed to sign out");
+
+      toast.success("Signed out successfully");
+
+      router.push("/sign-in");
+      router.refresh();
+    } catch (error) {
+      toast.error("Failed to sign out, please try again later");
+    }
+  };
+
+  return { signOut };
+};
